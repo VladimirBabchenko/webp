98414d718ec52428e10d193f1c8040c4775a0b87
VladimirBabchenko <mgrinevitch@gmail.com>
2018-02-19 00:14:43 +0200

    first

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const NODE_ENV = process.env.NODE_ENV || "development";
+const webpack = require("webpack");
+
+module.exports = {
+    entry: "./home.js",
+    output: {
+        filename: "build.js",
+        library: "home"
+    },
+
+    watch: NODE_ENV == "development",
+
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+
+    devtool: NODE_ENV == "development" ? "cheap-inline-module-source-map" : null,
+
+    plugins: [
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        })
+    ]
+};
